af542102682c420ef229e40df0513119f247f099
ftsujikawa <readme627@gmail.com>
2020-02-29 21:41:14 +0900

    第5.2章終了

---
+++ chap05/AutoBoxing.java
@@ -0,0 +1,12 @@
+package chap05;
+
+public class AutoBoxing {
+    public static void main(String... args) {
+        Long result = 0L;
+
+        for (var i = 1; i < 10000; i++) {
+            result += i;
+        }
+        System.out.println(result);
+    }
+}
